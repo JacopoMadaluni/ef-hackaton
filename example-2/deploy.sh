d5abcd6baa2c4415e2d8c2560f74dc415d016f36
JacopoMadaluni <jacopo.madaluni@gmail.com>
2023-09-13 22:41:57 +0100

    WIP

---
+++ example-2/deploy.sh
@@ -0,0 +1,9 @@
+resourceGroupName=eft-resource-group
+appName=eft-as-main
+zipFile=source.zip
+
+# az webapp deployment source config-zip --resource-group $resourceGroupName --name $appName --src $zipFile
+az webapp deploy \
+    --name $appName \
+    --resource-group $resourceGroupName \
+    --src-path $zipFile
